d01d57a2683a936c376f95f3365b68bd214d4c87
Vitaliy <theyamshikov@yandex.ru>
2019-10-25 23:28:46 +0600

    5555

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var build = require('./build/script/build');
+var build = require('./gulp/script/build');
 
 gulp.task('clean', build.clean);
 gulp.task('build-dev', build.dev);
